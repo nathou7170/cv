156ffb051281e358a1c95fa51c70b51affd4f34c
Nathanael <nath.vincent7@gmail.com>
2021-03-17 16:18:43 +0100

    ajout project

---
+++ scss/test.js
@@ -0,0 +1,13 @@
+function fizzBuzz(n){
+   for(var n=1; n<=15; i++){
+     if (i % 3 === 0 && i % 5 ===0){
+       console.log("fizzbuzz");
+     }
+     if else(i % 3 === 0 ){
+       console.log("fizz");
+     }else (i % 5 === 0 ){
+       console.log("buzz");
+     }
+   }
+}
+
